67632874f3b75629142732b1d197116423810fc8
erksoft <erksoft@PROGRAMADOR03>
2016-02-01 11:08:40 -0430

    Finalizado generación de facturas por productos perdidos

--- rental_calendar/wizard/rental_invoice_missing.py
+++ rental_calendar/wizard/rental_invoice_missing.py
@@ -17,6 +17,55 @@ class rental_invoice_missing(osv.osv_memory):
             'description': fields.char('Descripción'),
         }
 
+    def get_order_by_rental(self, cr, uid, rental_ids, rental_brw, context = None):
+        sales = []
+        for rental in rental_brw:
+            if rental.state == "out" and rental.in_state != "cancel":
+                sale_id = rental.start_order_id
+                if sale_id not in sales:
+                    sales.append(sale_id)
+        return sales
+
+    def _create_rental_missing_invoice(self, cr, uid, order, invoice_lines, context=None):
+        """Prepare the dict of values to create the new invoice for a
+           sales order. This method may be overridden to implement custom
+           invoice generation (making sure to call super() to establish
+           a clean extension chain).
+
+           :param browse_record order: sale.order record to invoice
+           :param list(dict) line: list of dictionaries of invoice lines values that must be
+                                  attached to the invoice
+           :return: create_id
+        """
+        if context is None:
+            context = {}
+        invoice_obj = self.pool.get('account.invoice')
+        journal_ids = self.pool.get('account.journal').search(cr, uid,
+            [('type', '=', 'sale'), ('company_id', '=', order.company_id.id)],
+            limit=1)
+        if not journal_ids:
+            raise osv.except_osv(_('Error!'),
+                _('Please define sales journal for this company: "%s" (id:%d).') % (order.company_id.name, order.company_id.id))
+        invoice_vals = {
+            'name': order.client_order_ref or '',
+            'origin': order.name,
+            'type': 'out_invoice',
+            'reference': order.client_order_ref or order.name,
+            'account_id': order.partner_invoice_id.property_account_receivable.id,
+            'partner_id': order.partner_invoice_id.id,
+            'journal_id': journal_ids[0],
+            'invoice_line': invoice_lines,
+            'currency_id': order.pricelist_id.currency_id.id,
+            'comment': order.note,
+            'payment_term': order.payment_term and order.payment_term.id or False,
+            'fiscal_position': order.fiscal_position.id or order.partner_invoice_id.property_account_position.id,
+            'date_invoice': context.get('date_invoice', False),
+            'company_id': order.company_id.id,
+            'user_id': order.user_id and order.user_id.id or False,
+            'section_id' : order.section_id.id
+        }
+        return invoice_obj.create(cr, uid, invoice_vals, context=context)
+
     def _prepare_rental_missing_invoice_line(self, cr, uid, line, context=None):
         """Prepare the dict of values to create the new invoice line for a
            sales order line. This method may be overridden to implement custom
@@ -39,7 +88,7 @@ class rental_invoice_missing(osv.osv_memory):
         uos_id = product_id.uom_id.id
         pu = 0.0
         if uosqty:
-            pu = round(product_id.list_price * uosqty, self.pool.get('decimal.precision').precision_get(cr, uid, 'Product Price'))
+            pu = round(product_id.list_price, self.pool.get('decimal.precision').precision_get(cr, uid, 'Product Price'))
         fpos = line.order_id.fiscal_position or False
         account_id = self.pool.get('account.fiscal.position').map_account(cr, uid, fpos, account_id)
         if not account_id:
@@ -58,7 +107,6 @@ class rental_invoice_missing(osv.osv_memory):
             'invoice_line_tax_id': [(6, 0, [x.id for x in line.tax_id])],
             'account_analytic_id': line.order_id.project_id and line.order_id.project_id.id or False,
         }
-
         return res
     
     def generate_missing_invoice(self,cr,uid,ids,context=None):
@@ -66,12 +114,22 @@ class rental_invoice_missing(osv.osv_memory):
             context = {}
         rental_ids = context.get("active_ids",False)
         if rental_ids:
-            sale_obj = self.pool.get('sale.order')
             rental_obj = self.pool.get('sale.rental')
-            sale_line_obj = self.pool.get('sale.order.line')
-            for rental in rental_obj.browse(cr,uid,rental_ids):
-                if rental.state == "out":
-                    print '***************',sale_line_obj._prepare_rental_missing_invoice_line(cr, uid, rental.start_order_line_id, context = context)
+            rental_brw = rental_obj.browse(cr,uid,rental_ids)
+            sales = self.get_order_by_rental(cr, uid, rental_ids, rental_brw, context = context)
+            if sales:
+                for sale in sales:
+                    inv_lines = []
+                    moves = []
+                    for rental in rental_brw:
+                        if rental.state == "out" and rental.in_state != "cancel":
+                            inv_line_vals = self._prepare_rental_missing_invoice_line(cr, uid, rental.start_order_line_id, context = context)
+                            inv_lines.append((0,0,inv_line_vals))
+                            moves.append(rental.in_move_id.id)
+                    if inv_lines:
+                        invoice_id = self._create_rental_missing_invoice(cr, uid, sale, inv_lines, context = context)
+                        if invoice_id:
+                            self.pool.get('stock.move').action_cancel(cr, uid, moves, context)
         else:
             raise osv.except_osv(_('Error'), _('Seleccione al menos un registro!'))
 
