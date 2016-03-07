08609cbf8314020eca3d44f280231db397c2ab8d
rodrigueza <rodrigueza@erksoft.com.ve>
2016-03-07 16:43:47 -0430

    Modificado report_invoice.xml para sinsumos_custom... Se agrego campo funcional picking_id en account_invoice

--- sinsumos_custom/account_invoice.py
+++ sinsumos_custom/account_invoice.py
@@ -21,9 +21,22 @@ class sinsumos_account_invoice(osv.osv):
             res[invoice.id] = number_to_letter.to_word(invoice.amount_total, mi_moneda = 'VEF')
         return res
 
+    def _get_picking(self, cr, uid, ids, field_name, arg, context=None):
+        context = context or {}
+        res = {}.fromkeys(ids, 0)
+        for invoice in self.browse(cr, uid, ids, context=context):
+            if invoice.origin:
+                picking = self.pool.get('stock.picking')
+                picking_id = picking.search(cr, uid, [('origin','=',invoice.origin)])
+                if picking_id:
+                    res[invoice.id] = picking_id[0]
+        return res
+
     _columns = {
                 'items': fields.function(_get_items, type='integer',
                         string="Items",store=True),
                 'amount_total_string': fields.function(_number_to_string, type='char',
                         string="Amount Total String",store=False),
+                'picking_id': fields.function(_get_picking, type='many2one', relation='stock.picking', string='Albarán',
+                            store=True, readonly=True),
     }
