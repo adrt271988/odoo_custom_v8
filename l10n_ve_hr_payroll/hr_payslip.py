938bad4127990899f00e143feb96c5dde68936bf
adrt271988 <adrt271988@gmail.com>
2016-04-03 18:53:06 -0430

    Modificado onchange_contract_id para traer el periodo de pago... Agregada estructura salarial para las retenciones

--- l10n_ve_hr_payroll/hr_payslip.py
+++ l10n_ve_hr_payroll/hr_payslip.py
@@ -25,15 +25,38 @@ from datetime import date
 from datetime import datetime
 from datetime import timedelta
 from dateutil import relativedelta
-
 from openerp import api, tools
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
-
 from openerp.tools.safe_eval import safe_eval as eval
 
+days_schedule_pay = {
+        'monthly': 30,
+        'quarterly': 90,
+        'semi-annually': 180,
+        'annually': 365,
+        'weekly': 7,
+        'bi-weekly': 15,
+        'bi-monthly': 60,
+    }
+
 class l10n_ve_hr_payslip(osv.osv):
+
+    def onchange_contract_id(self, cr, uid, ids, date_from, date_to, employee_id=False, contract_id=False, context=None):
+        res = super(l10n_ve_hr_payslip, self).onchange_contract_id(cr, uid, ids, date_from,\
+                        date_to, employee_id, contract_id, context=context)
+        if "value" in res:
+            if "contract_id" in res['value']:
+                contract_id = res['value']['contract_id']
+                contract_brw = self.pool.get('hr.contract').browse(cr, uid, contract_id, context=context)
+                period_days = days_schedule_pay.get(contract_brw.schedule_pay)
+                if period_days:
+                    date = time.strftime('%Y-%m-01')
+                    date_from = datetime.strptime(date, "%Y-%m-%d")
+                    end_date = date_from + timedelta(days=(period_days - 1))
+                    res['value'].update({'date_to':end_date})
+        return res
 
     _inherit = 'hr.payslip'
     _columns = {
