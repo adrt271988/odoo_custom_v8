6dd28c759df2ee29eaec396dfa52b0c298b528be
adrt271988 <adrt271988@gmail.com>
2016-04-07 21:56:46 -0430

    Agregada dependencia document_url a modulo lalita_reservation

--- lalita_reservation/__openerp__.py
+++ lalita_reservation/__openerp__.py
@@ -25,7 +25,7 @@
     'summary': 'Registro para Viajeros',
     'description': 'Gestión del Registro de Viajeros y envío de los partes a la Guardia Civil',
     'author': 'Jeronimo Spotorno, Alexander Rodriguez <adtr271988@gmail.com>',
-    'depends': ['base','document','mail','sales_crm_custom'],
+    'depends': ['base','document','mail','sales_crm_custom','document_url'],
     'application': True,
     'installable': True,
     'data':[
