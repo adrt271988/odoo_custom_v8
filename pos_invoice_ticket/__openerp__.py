b3e06c743a91412d513ec9264702280e1b3cb6af
Alexander Rodriguez <alexander@localhost.localdomain>
2016-05-24 22:34:34 -0400

    Agregado modulo pos_invoice_ticket para frontend de point_of_sale

---
+++ pos_invoice_ticket/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PoS Invoice Ticket',
+    'version': '1.0',
+    'category': 'Point Of Sale',
+    'summary': 'Print Invoice as ticket in Point of Sale',
+    'description': """
+Main Features
+-------------
+* Add a new button to print invoice as ticket in Point of Sale
+    """,
+    'author': 'Jose Suniaga <suniagajose@gmail.com>',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/view.xml'
+    ],
+    'qweb': [
+        'static/src/xml/invoice.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ pos_invoice_ticket/static/src/js/main.js
@@ -0,0 +1,7 @@
+openerp.pos_invoice_ticket = function(instance){
+
+    var module = instance.point_of_sale;
+
+    openerp_pos_invoice_ticket(instance,module);
+
+};
